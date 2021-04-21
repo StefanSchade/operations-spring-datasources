aba87fb84a7bde2b61814d5b2ee61e7b31269d24
Dr. Stefan Schade <dr_stefan_schade@yahoo.com>
2021-04-21 08:09:23 +0200

    initial commit

---
+++ src/main/java/com/example/operationsspringdatasources/OperationsSpringDatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.operationsspringdatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsSpringDatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsSpringDatasourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/operationsspringdatasources/OperationsSpringDatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.operationsspringdatasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsSpringDatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
